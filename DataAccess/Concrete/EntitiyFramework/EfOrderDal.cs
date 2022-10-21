2dfef676f9fd4709db4b9d33506881387391cf24
Enes <77552232+enesgurel@users.noreply.github.com>
2022-10-21 13:35:36 +0300

    Code Refactoring

--- DataAccess/Concrete/EntitiyFramework/EfOrderDal.cs
+++ DataAccess/Concrete/EntitiyFramework/EfOrderDal.cs
@@ -1,14 +1,14 @@
-﻿using Core.DataAccess.EntityFramework;
+﻿using Core.DataAccess;
+using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntitiyFramework
 {
-    public class EfCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
+    public class EfOrderDal:EfEntityRepositoryBase<Order, NorthwindContext>, IOrderDal
     {
 
     }
