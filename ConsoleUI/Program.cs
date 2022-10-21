4d9e72c06d4d9d9a29c6eaf973bcc919658eb393
Enes <77552232+enesgurel@users.noreply.github.com>
2022-10-21 14:11:55 +0300

    Added DTOs

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,14 +11,20 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //DTO : Data Transformation Object
             //ProductTest();
             //IoC
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
         }
 
         private static void ProductTest()
@@ -26,9 +32,9 @@ namespace ConsoleUI
             //ProductManager productManager = new ProductManager(new InMemoryProductDal());
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
             }
         }
     }
